384c054db93b15c71eb1b7057be90cf23824e3a6
prajwalgunjal <prajwalgunjal8@gmail.com>
2024-02-29 21:44:33 +0530

    AddressBook

--- SimpleArray.py
+++ SimpleArray.py
@@ -6,3 +6,14 @@ print(array)
 
 array_Diff = [1,"abcd",1.2]
 print(array_Diff)
+
+#if i want to inset each element then code will be like 
+
+new_array =[]
+numOfelement = int(input("Enter the size of the array : -"))
+
+for i in range (new_array):
+    userip =input(f"Enter element {i+1} ")
+    new_array.append(userip)
+
+print(new_array)
